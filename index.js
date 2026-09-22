897fe82613beec7e052b20bb43155381fd6284cd
Devika <devika999988@gmail.com>
2026-09-22 23:43:41 +0530

    first commit of javascript

---
+++ index.js
@@ -0,0 +1,43 @@
+//variable kaise bnate h
+let name = "Devika";
+//print krna variable ko
+console.log(name);
+
+let age = 20;
+
+console.log (age);
+
+//dono ko sath m print... console.log (name, age);
+
+//kya hm variables ki values ko change kr skte h?
+age = 30;
+
+console.log(age);       //yesssss
+
+
+//2nd method to make variable
+const account = 2743;
+
+console.log(account);
+
+/*account = 93840;
+
+console.log (accoumt); */                       //ye wale variable ki value change ni kr skte...error
+
+
+//OLD METHOD TO DECLARE VARIABLE
+/*
+var a = 10;
+console.log(a);     prints 10
+
+var b = 20;
+var b = 30;
+console.log (b);        prints 30....dikkt-- ek hi variable ko multiple times declare kr skte h..let se ek variable ko sirf ek br declare kr skte h
+
+if (true) {
+    var c = 10;
+}
+
+console.log(c);         valid sirf if  ke andr hona chahiye tha lekin bhar b h.. scope se bhr... not desirable
+
+*/

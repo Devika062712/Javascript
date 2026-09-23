efab76360e1ea2fa82373d16815037dbf873e19b
Devika <devika999988@gmail.com>
2026-09-23 23:48:46 +0530

    added today's work

--- index.js
+++ index.js
@@ -41,3 +41,95 @@ if (true) {
 console.log(c);         valid sirf if  ke andr hona chahiye tha lekin bhar b h.. scope se bhr... not desirable
 
 */
+
+/*
+datatypes-- primitive and non primitive
+
+primitive --> number, string, boolean, undefined, null, bigint, symbol
+non-primitive --> array, object, function
+*/
+
+//number.....8byte..64 bit
+let num = 10;       //10.1 also
+console.log(num);
+
+//string
+let s = "sentence or word";     //can also use '
+console.log(s);
+
+//boolean
+let login = true;
+console.log(login);
+
+//undefined         .....const ke case m aise ni ho skta.. value sath sath hi assign krni pdti h
+let user;       //declare krdia variable ko pr koi value ni dali
+console.log(user);
+
+//bigint....no. greater than 8 bytes
+let n = 68757865456789n;        //last m n likhna hota h bigint batane ke lie
+console.log(n);
+
+//null
+let weather = null;     //reprents intentional absence of any object value....exists but cant fetch now..not defined me exist hi ni krta
+console.log(weather);
+
+//symbol     ...unique chizo ko create krne ke lie use hota h
+const id1 = Symbol("id");
+console.log(id1);   
+//yha dono equal nhi aaengi  
+const id2 = Symbol("id");
+console.log(id1 == id2);
+
+
+//non primitive
+//array
+let arr = [34, 434, 4, 'Devika', true];
+console.log(arr);
+
+//creating object       ....key value pair m meaningful data deta h
+let obj = {
+    name : "Devika",
+    account : 783292,
+    age : 19,
+    category : "OBC",
+}
+
+//function
+function add() {
+    return ("Hello");
+}
+
+console.log(add());
+
+//OR
+
+function add1 () {
+    console.log("Hello");
+}
+
+add1();
+
+//js m function ko kisi variable m store kra skte h
+
+let d = function add2() {
+console.log("YAY");
+}
+
+console.log(d);     //aiser ye bataega ki variable ke andr fun h but jo actual print krana h uske lie d(); aise likhna hoga
+
+
+
+//kisi b variable ka type pta krne ke lie..
+let e = 3892;
+console.log(typeof(e));
+
+console.log(typeof(add()));     //fun se milne wali return value ka type
+
+console.log(typeof(add1()));
+
+console.log(typeof(add));       //fun ka type
+
+console.log(typeof(weather));       //yha weather null h pr ye null ki jgh iske type ko obj bol deta h and this is the biggest bug of js
+
+//obj apna type obj bataega.. aur array b apna type obj hi bataega
+
